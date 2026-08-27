65bbb1a13ae2575f3eaeed6374eb2e1d9f5db732
ip-ram <ipramrabota@gmail.com>
2026-08-28 01:03:29 +0500

    cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -37,23 +37,32 @@ pub fn run(conf: &BackupConfig) -> Result<&'static str, std::io::Error> {
 
         next_run += interval;
 
-        let _result = match backup_dir(conf.get_data_dir(), conf.get_backup_dir()) {
-            Ok(status) => match print_log_in_log_dir(conf.get_backup_dir(), status) {
+        let date = Local::now().to_string();
+
+        let _result = match backup_dir(conf.get_data_dir(), conf.get_backup_dir(), &date) {
+            Ok(status) => match print_log_in_log_dir(conf.get_backup_dir(), status, &date) {
                 Ok(str) => Ok(str),
                 Err(err) => Err(err),
             },
-            Err(status) => match print_log_in_log_dir(conf.get_backup_dir(), &status.to_string()) {
-                Ok(str) => Ok(str),
-                Err(err) => Err(err),
-            },
+            Err(status) => {
+                match print_log_in_log_dir(conf.get_backup_dir(), &status.to_string(), &date) {
+                    Ok(str) => Ok(str),
+                    Err(err) => Err(err),
+                }
+            }
         };
     }
 }
 
-fn backup_dir(from: String, to: String) -> Result<&'static str, fs_extra::error::Error> {
+fn backup_dir(
+    from: String,
+    to: String,
+    date: &str,
+) -> Result<&'static str, fs_extra::error::Error> {
     let mut options = CopyOptions::new();
+    options.copy_inside = true;
 
-    options.copy_inside = false;
+    let to = format!("{}/{}", to, date);
 
     match copy(from, to, &options) {
         Result::Ok(_int) => Ok("backup success"),
@@ -61,7 +70,11 @@ fn backup_dir(from: String, to: String) -> Result<&'static str, fs_extra::error:
     }
 }
 
-fn print_log_in_log_dir(to: String, log_text: &str) -> Result<&'static str, std::io::Error> {
+fn print_log_in_log_dir(
+    to: String,
+    log_text: &str,
+    date: &str,
+) -> Result<&'static str, std::io::Error> {
     let backup_dir = to;
     let log_path = format!("{}{}", backup_dir, "/log.txt");
     let mut file = fs::OpenOptions::new()
@@ -70,9 +83,8 @@ fn print_log_in_log_dir(to: String, log_text: &str) -> Result<&'static str, std:
         .open(log_path)
         .unwrap();
 
-    let now_from_str = Local::now();
-    let log_output = now_from_str.to_string() + log_text;
-    match writeln!(file, "{log_output}") {
+    let log_output = format!("{}, {}", date, log_text);
+    match writeln!(file, " {log_output}") {
         Ok(_) => Ok("success"),
         Err(err) => Err(err),
     }
